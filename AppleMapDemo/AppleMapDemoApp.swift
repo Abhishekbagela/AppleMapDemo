628873902d0e0fd6d1786b65b3afbcb62dca35ed
Abhishek Bagela <abhibagela2015@gmail.com>
2023-02-01 20:05:34 +0530

    Initial Commit

---
+++ AppleMapDemo/AppleMapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMapDemoApp.swift
+//  AppleMapDemo
+//
+//  Created by Abhishek Bagela on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleMapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
